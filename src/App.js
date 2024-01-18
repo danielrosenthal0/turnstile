c43c7cbbf19003fe147c9793ebfc8668d1fad0c5
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-01-18 15:43:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+   <div className={styles.content}>
+    <p className={styles.title}>Turnstile</p>
+  </div>
+  );
+}
+
+export default App;
