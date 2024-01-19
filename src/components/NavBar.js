be8bed473245092dc62e3b33df778f7e0a73ebe7
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-01-19 13:20:57 -0500

    nav auth context

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
+import { AuthContext } from "../services/AuthContext";
 
 const NavBar = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className={styles.content}>
       <Link to="/">
         <p className={styles.logo}>T</p>
       </Link>
-      <div className={styles.pages}>
-        <Link to="/sign-up">
-          <div>Sign Up</div>
-        </Link>
-        <Link to="/sign-in">
-          <div>Sign In</div>
-        </Link>
-        <Link to="/profile">
-          <div>Profile</div>
-        </Link>
-      </div>
+
+      {user ? (
+        <div className={styles.pages}>
+          <Link to="/profile">
+            <div>Profile</div>
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.pages}>
+          <Link to="/sign-up">
+            <div>Sign Up</div>
+          </Link>
+          <Link to="/sign-in">
+            <div>Sign In</div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
