e11c9c7a7b3c3257705cde8ac96ee400c2a1de75
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-01-19 12:21:39 -0500

    sign in

--- src/services/auth.js
+++ src/services/auth.js
@@ -45,6 +45,26 @@ export function confirmSignUp(username, code) {
 
 export function signIn(username, password) {
   // Sign in implementation
+  return new Promise ((resolve, reject) => {
+    const authenticationDetails = new AuthenticationDetails({
+      Username: username,
+      Password: password,
+    })
+
+    const cognitoUser = new CognitoUser({
+      Username: username,
+      Pool: userPool,
+    })
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => {
+        resolve(result)
+      },
+      onFailure: (error) => {
+        reject(error)
+      }
+    })
+  })
 }
 
 export function forgotPassword(username) {
