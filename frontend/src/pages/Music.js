6701dd0eec66f10f7fe8d460c2016165f43243af
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-01-24 13:55:44 -0500

    files successfully uploaded

--- frontend/src/pages/Music.js
+++ frontend/src/pages/Music.js
@@ -1,15 +1,27 @@
 import { useRef } from 'react';
+import { S3 } from 'aws-sdk';
+import AWS from 'aws-sdk';
+import { awsConfig } from '../services/awsConfig';
 import styles from './Music.module.css';
+
+AWS.config.update(awsConfig);
 
 const Music = () => {
   const fileInputRef = useRef();
+  const s3 = new S3();
 
   const handleFileUpload = async () => {
     const file = fileInputRef.current.files[0];
 
     try {
-      const key = `${Date.now()}_${file.name}`;
-      await Storage.put(key, file, { level: 'public'});
+      const params = {
+        Bucket: 'turnstile-music',
+        Key: `${Date.now()}_${file.name}`,
+        Body: file,
+        ACL: 'public-read',
+      };
+
+      await s3.upload(params).promise();
       console.log('File uploaded successfully');
     } catch (error) {
       console.error('Error uploading file:', error);
--- frontend/src/services/awsConfig.js
+++ frontend/src/services/awsConfig.js
@@ -1,16 +1,16 @@
 import AWS from 'aws-sdk';
 
-AWS.config.update({
+const awsConfig = {
   region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.REACT_APP_ACCESS_KEY,
     secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY
   },
-});
+};
 
 const s3Config = {
   bucket: 'turnstile-music',
   region: 'us-east-1'
 };
 
-export { AWS, s3Config };
+export { awsConfig, s3Config };
