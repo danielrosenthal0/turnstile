e80ccca66256d224b5b484726213e483220b41ae
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-01-24 13:58:01 -0500

    only mp3 accepted

--- frontend/src/pages/Music.js
+++ frontend/src/pages/Music.js
@@ -32,12 +32,11 @@ const Music = () => {
     <div className={styles.content}>
       <h1>Music</h1>
       <h3>Upload your music here</h3>
-      <input type="file" ref={fileInputRef}/>
+      <input type="file" ref={fileInputRef} accept="audio/mpeg, audio/wav" />
       <button onClick={handleFileUpload}>Upload</button>
       <h2>Browse some of the most popular uploads</h2>
-
     </div>
-  )
+  );
 }
 
 export default Music;
