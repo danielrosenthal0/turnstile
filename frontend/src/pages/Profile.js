ad91cf39c4aa742b48a93cdcb7bdefdf8a2a9d3b
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-01-25 18:46:05 -0500

    trying to list songs

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../services/AuthContext";
+import { S3 } from "aws-sdk";
 import styles from './Profile.module.css';
 
 const getAccountLabel = (accountType) => {
@@ -13,6 +14,27 @@ const getAccountLabel = (accountType) => {
 
 const Profile = () => {
   const { user, signOut } = useContext(AuthContext);
+  const [userFiles, setUserFiles] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      const s3 = new S3();
+
+      const params = {
+        Bucket: 'turnstile-music',
+        Prefix: `${user.username}/`,
+      };
+
+      s3.listObjectsV2(params, (error, data) => {
+        if (error) {
+          console.error('Error listing user files:', error);
+        } else {
+          const files = data.Contents.map(item => item.Key);
+          setUserFiles(files);
+        }
+      })
+    }
+  }, [user]);
 
   return (
     <div className={styles.content}>
@@ -22,6 +44,17 @@ const Profile = () => {
         <p>Username: {user.username}</p>
         <p>Email: {user.email}</p>
         <p>Account type: {getAccountLabel(user["custom:UserType"])}</p>
+        <h3>Uploaded music</h3>
+        {userFiles.length > 0 ? (
+          <ul>
+            {userFiles.map((file, index) => (
+              <li key={index}>{file.split('/').pop()}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No uploaded music</p>
+        )}
+        
         </div>
       )}
       <button onClick={signOut}>Sign out</button>
