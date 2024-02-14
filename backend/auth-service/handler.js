9d2284f813ac0465072a442267e11cf5bad07727
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-02-14 15:02:57 -0500

    fixed signup, deployed to amplify

--- backend/auth-service/handler.js
+++ backend/auth-service/handler.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const { REGION, USER_POOL_ID, CLIENT_ID } = require('./cognitoEnv');
 const { CognitoUserPool, CognitoUserAttribute } = require('amazon-cognito-identity-js');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const USER_POOL_ID = process.env.USER_POOL_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
 
 const poolData = {
   UserPoolId: USER_POOL_ID,
@@ -13,37 +15,66 @@ const poolData = {
 
 const userPool = new CognitoUserPool(poolData);
 
-
 async function signUp(event, context) {
-  const { username, email, password, userType } = JSON.parse(event.body);
+  // Handle OPTIONS request
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // This is the important line
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,X-Amzn-Trace-Id',
+        'Access-Control-Allow-Methods': 'POST,OPTIONS',
+      },
+    };
+  }
 
-  return new Promise((resolve, reject) => {
-    userPool.signUp(
-      username,
-      password,
-      [
-        new CognitoUserAttribute({ Name: 'email', Value: email }),
-        new CognitoUserAttribute({ Name: 'custom:UserType', Value: userType }),
-      ],
-      [],
-      (err, result) => {
+  console.log(event.body);
+  try {
+    const { username, email, password, userType } = JSON.parse(event.body);
+
+    const attributes = [
+      new CognitoUserAttribute({ Name: 'email', Value: email }),
+      new CognitoUserAttribute({ Name: 'custom:UserType', Value: userType }),
+    ];
+
+    const result = await new Promise((resolve, reject) => {
+      userPool.signUp(username, password, attributes, [], (err, result) => {
         if (err) {
-          return reject(err);
+          reject(err);
+        } else {
+          resolve(result);
         }
+      });
+    });
 
-        resolve({
-          statusCode: 200,
-          body: JSON.stringify({
-            user: result,
-          }),
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true,
-          },
-        });
-      }
-    );
-  });
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // This is the important line
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+        user: result,
+      }),
+      
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', // This is the important line
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  }
+}
+
+module.exports = {
+  signUp,
 };
-
-exports.handler = signUp;
