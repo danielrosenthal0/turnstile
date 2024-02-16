e0f8e3b212c2f872b0dcde11faf70bd89f9e5fb5
danielrosenthal0 <daniel.b.rosenthal@gmail.com>
2024-02-16 09:52:22 -0500

    confirm and resend code works

---
+++ backend/auth-service/handlers/signUp.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const { CognitoUserPool, CognitoUserAttribute } = require('amazon-cognito-identity-js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const USER_POOL_ID = process.env.USER_POOL_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
+
+const poolData = {
+  UserPoolId: USER_POOL_ID,
+  ClientId: CLIENT_ID,
+};
+
+const userPool = new CognitoUserPool(poolData);
+
+async function handler(event, context) {
+  const allowedOrigins = ['https://dev.d5quvta5fj0rx.amplifyapp.com', 'https://prod.d5quvta5fj0rx.amplifyapp.com', 'http://localhost:3000'];
+  const origin = event.headers.origin;
+  if (!allowedOrigins.includes(origin)) {
+    return {
+      statusCode: 403,
+      body: 'Forbidden',
+    };
+  }
+  // Handle OPTIONS request
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,X-Amzn-Trace-Id',
+        'Access-Control-Allow-Methods': 'POST,OPTIONS',
+      },
+    };
+  }
+
+  try {
+    const { username, email, password, userType } = JSON.parse(event.body);
+
+    const attributes = [
+      new CognitoUserAttribute({ Name: 'email', Value: email }),
+      new CognitoUserAttribute({ Name: 'custom:UserType', Value: userType }),
+    ];
+
+    const result = await new Promise((resolve, reject) => {
+      userPool.signUp(username, password, attributes, [], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+        user: result,
+      }),
+      
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  }
+}
+
+module.exports = {
+  handler,
+};
